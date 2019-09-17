cc59508d331a7a493796a96d7c00c6057b9cd478
Ed <ed.lozano.developer@gmail.com>
2019-09-17 12:23:31 -0400

    Implementing Versioning - Versioning by passing a parameter and by passing the header key X-API-VERSION.

--- src/main/java/com/demo/eduardo/demo4/versioning/PersonVersioningController.java
+++ src/main/java/com/demo/eduardo/demo4/versioning/PersonVersioningController.java
@@ -16,4 +16,26 @@ public class PersonVersioningController {
 	public PersonV2 getPersonV2() {
 		return new PersonV2(new Name("Ed", "Lozano"));
 	}
+	
+	// Versioning by passing a parameter in the URI
+	@GetMapping(value="/person/by-param", params="version=1")
+	public PersonV1 getPersonByParamV1() {
+		return new PersonV1("Ed Lozano by params");
+	}
+	
+	@GetMapping(value="/person/by-param", params="version=2")
+	public PersonV2 getPersonByParamV2() {
+		return new PersonV2(new Name("Ed by params", "Lozano by params"));
+	}
+	
+	// Versioning by passing a header in the request
+	@GetMapping(value="/person/by-header", headers="X-API-VERSION=1")
+	public PersonV1 getPersonByHeaderV1() {
+		return new PersonV1("Ed Lozano by headers");
+	}
+	
+	@GetMapping(value="/person/by-header", headers="X-API-VERSION=2")
+	public PersonV2 getPersonByHeaderV2() {
+		return new PersonV2(new Name("Ed by header", "Lozano by header"));
+	}
 }
