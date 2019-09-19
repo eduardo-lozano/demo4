671b645d58e9caef5dfd14aa2a63bb7ac2f8d856
Ed <ed.lozano.developer@gmail.com>
2019-09-19 12:49:39 -0400

    Implementing JPA - Create a SQL file with SQL insert queries that populate dummy records, those will be populated automatically into the corresponding table when restarting the full application.

--- src/main/java/com/demo/eduardo/demo4/user/SomeUser.java
+++ src/main/java/com/demo/eduardo/demo4/user/SomeUser.java
@@ -2,11 +2,18 @@ package com.demo.eduardo.demo4.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+// This declares an DB entity managed with JPA
+@Entity
 public class SomeUser {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min=2, message="This message would be shown to notice that the name must have 2 or more characters.")
 	private String name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO SOME_USER VALUES(1, sysdate(), 'First Person')
+INSERT INTO SOME_USER VALUES(2, sysdate(), 'Second Person')
+INSERT INTO SOME_USER VALUES(3, sysdate(), 'Third Person')
